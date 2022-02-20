e1db8a65c010c7230a736369972fecfb1bbdf98b
Josh P. Sawyer <josh.p.sawyer@gmail.com>
2022-02-20 10:04:22 -0500

    major restructuring

--- theme/webassets-external/225da15f283bd38cbdcd7a3fdd5552ba_main.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('#close-mobile-menu').addEventListener('click', () => {
-    document.querySelector('#mobile-menu').classList.remove('mobile-menu-active');
-}); 
-
-document.querySelector('#open-mobile-menu').addEventListener('click', () => {
-    document.querySelector('#mobile-menu').classList.add('mobile-menu-active');
-}); 
